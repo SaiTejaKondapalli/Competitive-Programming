d9a0fc703815794050413164b871a08667f5ad01
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-14 14:03:02 +0530

    Jul 14  14:03:02

--- 03-recursion-Java/recursion.java
+++ 03-recursion-Java/recursion.java
@@ -1,5 +1,10 @@
 class recursion {
-	public int get_fib(int value){
-	    return -1; 
+	public int get_fib(int value) {
+		if (value <= 1) {
+			return value;
+		}
+		return get_fib(value - 1) + get_fib(value - 2);
+	}
+	public static void main(String[] args) {
 	}
 }
