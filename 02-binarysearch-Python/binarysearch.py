38bb5e9cee888eae62fa3fb92159119d6d763f3b
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-17 16:40:56 +0530

    Jul 17  16:40:56

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -5,11 +5,22 @@ Your function should take two inputs:
 a Python list to search through, and the value
 you're searching for.
 Assume the list only has distinct elements,
-meaning there are no repeated values, and 
+meaning there are no repeated values, and
 elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
 def binary_search(input_array, value):
     # Your code goes here
-    pass
+    low = 0
+    high = len(input_array)
+    while low <= high:
+        mid = low+high//2
+        if value == input_array[mid]:
+            return mid
+        if value < mid:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
