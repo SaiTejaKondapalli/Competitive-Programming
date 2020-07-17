a0c3478045bd70af89d52e0a40b3e8deab274c0b
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-17 16:42:57 +0530

    Jul 17  16:42:57

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -18,7 +18,7 @@ def binary_search(input_array, value):
         mid = low+high//2
         if value == input_array[mid]:
             return mid
-        if value < mid:
+        if value < input_array[mid]:
             high = mid - 1
         else:
             low = mid + 1
