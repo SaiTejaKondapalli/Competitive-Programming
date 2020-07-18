3b16bc2f1b4bf5bbc4d3f8f349f49b2f66eba251
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-18 12:20:23 +0530

    Jul 18  12:20:23

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -1,5 +1,5 @@
-# Write the function rotatestring(s, k) that takes a string s and a possibly-negative integer k. 
-# If k is non-negative, the function returns the string s rotated k places to the left. 
+# Write the function rotatestring(s, k) that takes a string s and a possibly-negative integer k.
+# If k is non-negative, the function returns the string s rotated k places to the left.
 # If k is negative, the function returns the string s rotated |k| places to the right. So, for example:
 # assert(rotateString('abcd',  1) == 'bcda')
 # assert(rotateString('abcd', -1) == 'dabc')
@@ -7,5 +7,6 @@
 
 
 def fun_rotatestrings(s, n):
-	return s
+	return s[n%len(s):] + s[:n%len(s)]
+
 
