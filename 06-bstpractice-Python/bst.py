4f76dfeb2b071d282747f0021d6ed51a06031999
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-18 23:37:51 +0530

    Jul 18  23:37:51

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -15,27 +15,30 @@ class BST(object):
         else:
             if self.root.value < new_val:
                 if self.root.right is None:
-                    self.root.right.value = new_val
+                    self.root.right = Node(new_val)
                 else:
                     return BST.insert(self.root.right, new_val)
             else:
                 if self.root.left is None:
-                    self.root.left.value = new_val
+                    self.root.left = Node(new_val)
                 else:
-                    return self.insert(self.root.left, new_val)
+                    return BST.insert(self.root.left, new_val)
 
     def printSelf(self):
         # Your code goes here
         if self.root:
-            self.printSelf(self.root.left)
+            BST.printSelf(self.root.left)
             print(self.root.value)
-            self.printSelf(self.root.right)
+            BST.printSelf(self.root.right)
 
     def search(self, find_val):
         # Your code goes here
-        if self.root is None or self.root.value == find_val:
-            return self.root
+        # print(BST.printSelf(self))
+        if self.root == None:
+            return False
+        if self.root.value == find_val:
+            return True
         if self.root.value < find_val:
-            return self.search(self.root.left, find_val)
-        return self.search(self.root.right,find_val)
+            return BST.search(self.root.left, find_val)
+        return BST.search(self.root.right,find_val)
 
