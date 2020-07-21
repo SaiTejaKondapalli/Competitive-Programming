838ec6f5ad8388574f0f5b54fdbca65dda70c7ab
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-21 14:54:15 +0530

    matrix multiply

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,16 +5,19 @@
 
 import numpy as np
 def fun_matrixmultiply(m1, m2):
-    res = np.zeros([len(m1),len(m2[0])],dtype = int)
+    # res = np.zeros([len(m1),len(m2[0])],dtype = int)
+    # if len(m1[0]) != len(m2):
+    #     return None
+    # else:
+    #     for i in range(len(m1)):
+    #         for j in range(len(m2[0])):
+    #             for k in range(len(m2)):
+    #                 res[i][j] += m1[i][k] * m2[k][j]
+    #     print(res.tolist())
+    #     return res.tolist()
     if len(m1[0]) != len(m2):
         return None
-    else:
-        for i in range(len(m1)):
-            for j in range(len(m2[0])):
-                for k in range(len(m2)):
-                    res[i][j] += m1[i][k] * m2[k][j]
-        print(res.tolist())
-        return res.tolist()
+    return np.dot(m1,m2).tolist()
 
 
 
