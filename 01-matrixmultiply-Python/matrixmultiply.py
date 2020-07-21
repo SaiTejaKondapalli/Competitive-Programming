862ed31f5d10c1b7bb275e1724ce727e44cd1471
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-21 14:46:48 +0530

    Jul 21  14:46:47

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,14 +5,15 @@
 
 import numpy as np
 def fun_matrixmultiply(m1, m2):
-    res = np.zeros([m1.length,m2[0].length])
-    if m1[0].length != m2.length:
+    res = np.zeros([len(m1),len(m2[0])])
+    if len(m1[0]) != len(m2):
         return None
     else:
-        for i in range(m1.length):
-            for j in range(m2[0].length):
-                for k in range(m2.length):
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m2)):
                     res[i][j] += m1[i][k] * m2[k][j]
+        print(res.tolist())
         return res
 
 
