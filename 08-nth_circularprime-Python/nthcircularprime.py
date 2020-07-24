352b5c9521307f4be1cfec0e9b550371328696cd
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-24 20:37:04 +0530

    nth circular prime

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -12,15 +12,10 @@ def isprime(n):
 		return True
 	if n % 2 == 0:
 		return False
-	for i in range(3, int(math.sqrt(n)), 2):
+	for i in range(3, int(math.sqrt(n))+1, 2):
 		if n % i == 0:
 			return False
 	return True
-	# count = 0
-	# for i in range(1, n + 1):
-	# 	if n % i == 0:
-	# 		count += 1
-	# return count == 2
 def iscircularprime(n):
 	if "0" in str(n):
 		return False
