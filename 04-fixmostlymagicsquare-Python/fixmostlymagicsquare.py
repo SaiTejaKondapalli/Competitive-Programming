cfcd536cc45585d432f7e84533108606996dcdcf
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-07-26 20:52:34 +0530

    Jul 26  20:52:34

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -1,11 +1,22 @@
 # fixMostlyMagicSquare(L) [15 pts]
-# In this week's writing session, we wrote isMostlyMagicSquare(L). Here, say we have a mostly magic square L, but 
-# then we modify L by changing exactly one value in L so that it no longer is a mostly magic square. For this 
-# exercise, we assume we have just such a list L, and your task is to find and fix that change. So, given the list 
-# L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic 
+# In this week's writing session, we wrote isMostlyMagicSquare(L). Here, say we have a mostly magic square L, but
+# then we modify L by changing exactly one value in L so that it no longer is a mostly magic square. For this
+# exercise, we assume we have just such a list L, and your task is to find and fix that change. So, given the list
+# L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic
 # square.
 
 
 def fixmostlymagicsquare(L):
-	pass
 	# Your code goes here
+	s = []
+	for i in L:
+		s.append(sum(i))
+	index = s.index(min(s))
+	print(index)
+	print(s[index])
+	print(s[index],s[index+1])
+	diff = s[index] - s[index + 1]
+	print(diff)
+	s[index][-1] = s[index][-1] - diff
+	print(s)
+	return s
